ed2ea9fa734e484ec48390f0edf1e86221c3e171
Tiffany Brown <tiffany@webinista.com>
2024-03-03 20:12:26 -0800

    Update Input to support a size property.

--- src/Input.js
+++ src/Input.js
@@ -2,13 +2,22 @@ const Input = ({
   type,
   name,
   onChange = () => {},
+  defaultValue="",
   labelText="",
-  step=null
+  step=null,
+  size=25
 }) => {
   return (
     <>
       <label htmlFor={name}>{labelText}</label>
-      <input step={step} type={type} id={name} name={name} onChange={onChange} />
+      <input
+        step={step}
+        type={type}
+        id={name}
+        name={name}
+        onChange={onChange}
+        size={size}
+        defaultValue={defaultValue} />
     </>
   )
 }
